4faf5a68122ba0b4fc2465b3c3eaf90ace951a96
sk <saikiran8320.dav.@gmail.com>
2025-05-06 20:19:40 +0530

    server changed url

--- server.js
+++ server.js
@@ -1,23 +1,58 @@
+// const express = require("express");
+// const DBConnection = require("./config/DBconfig.js");
+// const { userRoutes } = require("./routes/user.route.js");
+// const { taskRoutes } = require("./routes/task.route.js");
+// const cors = require("cors");
+
+// require("dotenv").config();
+// const app = express();
+
+// const PORT = process.env.PORT || 5000;
+
+// DBConnection();
+// app.use(express.json());
+// app.use(cors());
+
+// app.use("/api/users", userRoutes);
+// app.use("/api/tasks", taskRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Working on Rendeer");
+// });
+
+// app.listen(PORT, () => console.log(`server running on PORT: ${PORT}`));
+
+
+require("dotenv").config();
 const express = require("express");
 const DBConnection = require("./config/DBconfig.js");
 const { userRoutes } = require("./routes/user.route.js");
 const { taskRoutes } = require("./routes/task.route.js");
 const cors = require("cors");
 
-require("dotenv").config();
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 DBConnection();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://your-vercel-project.vercel.app" 
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
+
 app.use(express.json());
-app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Working on Rendeer");
+  res.send("Working on Render");
 });
 
 app.listen(PORT, () => console.log(`server running on PORT: ${PORT}`));
+
